553192de3e24eb16c7a98a331a85862fb95dd8f4
Arek <kontakt@ohsofresh.pl>
2026-05-05 14:44:14 +0200

    Automatyczny deploy

--- resources/views/blocks/text-img.blade.php
+++ resources/views/blocks/text-img.blade.php
@@ -18,7 +18,7 @@ $sectionClass .= ' ' . $background;
 		<div class="__col grid grid-cols-1 lg:grid-cols-2 items-center gap-10">
 			@if (!empty($g_textimg['image']))
 			<div class="__img order1">
-				<img class="object-cover w-full __img radius-img min-h-80 h-full max-h-100" src="{{ $g_textimg['image']['url'] }}" alt="{{ $g_textimg['image']['alt'] ?? '' }}">
+				<img class="object-cover w-full __img radius-img h-120" src="{{ $g_textimg['image']['url'] }}" alt="{{ $g_textimg['image']['alt'] ?? '' }}">
 			</div>
 			@endif
 
